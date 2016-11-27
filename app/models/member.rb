96ddd9247059afd45e14123616f98e10001ace47
Alexander Sigatchov <asigatchov@gmail.com>
2016-11-27 14:06:05 +0300

    fix img

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ApplicationRecord
 
   belongs_to :team
-  has_attached_file :photo, styles: { medium: "250x300>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo, styles: { medium: "250x300>" }, default_url: "/system/members/default.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
